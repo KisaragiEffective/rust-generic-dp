308d7f0ccf64255a6a1510319de25ba76c49b670
KisaragiEffective <kisaragi.effective@gmail.com>
2022-06-17 03:36:11 +0900

    feat: more simple dp

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,73 @@ use crate::collecting::{Magma, ReduceByMagma, Reducer, Sum};
 use crate::dp_traits::DPOwned;
 use crate::perf::run_print_time;
 
+struct PartialTopDownDP<'dp, I, R, M: Magma<R>, Solver: Fn(I) -> PartialProblemState<I, R>> {
+    solver: Solver,
+    compose_by: M,
+    __phantoms: PhantomData<(&'dp (), I, R)>,
+}
+
+// Cache: CachePolicy<I, Rc<ProblemState<R, PartialProblemAnswerCombiner, I>>>,
+impl<
+    'dp,
+    I: Copy + Debug,
+    R: Copy + Debug,
+    M: Copy + Magma<R>,
+    Solver: Fn(I) -> PartialProblemState<I, R>,
+> DP<'dp, I, R> for PartialTopDownDP<'dp, I, R, M, Solver> {
+    fn dp(&'dp self, initial_index: I) -> R {
+        use crate::perf::run_print_time;
+        let solve_result_ref = (self.solver)(initial_index);
+        // println!("computed {initial_index:?}, {:?}", &solve_result_ref);
+        match solve_result_ref {
+            PartialProblemState::Intermediate { dependent } => {
+                // let dependent = dbg!(dependent);
+                let smaller_indexes = &dependent;
+                let len = smaller_indexes.len().into();
+                let mut buffer: Vec<MaybeUninit<R>> = Vec::with_capacity(len);
+                buffer.resize_with(len, || MaybeUninit::uninit());
+                let mut i = 0;
+                for smaller_index in smaller_indexes {
+                    let lp = self.dp(*smaller_index);
+                    buffer[i] = MaybeUninit::new(lp);
+                    i += 1;
+                }
+                let collected_values = buffer.into_iter().map(|a| unsafe { a.assume_init() }).collect::<Vec<_>>();
+                let (_, collected_values) = (initial_index, collected_values);
+                let reducer = ReduceByMagma::new(self.compose_by);
+                use crate::collecting::Reducer;
+                let reduce_result = reducer.reduce(collected_values.try_into().unwrap());
+                reduce_result
+            }
+            PartialProblemState::Base { base_result } => {
+                base_result.clone()
+            }
+        }
+
+    }
+}
+
+fn simple_dp<'dp, I: Copy, R: Copy, M: Copy + Magma<R>, Solver: Fn(I) -> PartialProblemState<I, R>>(
+    solver: Solver,
+    compose_by: M,
+) -> PartialTopDownDP<'dp, I, R, M, Solver> {
+    PartialTopDownDP {
+        solver,
+        compose_by,
+        __phantoms: Default::default()
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+enum PartialProblemState<I, R> {
+    Base {
+        base_result: R,
+    },
+    Intermediate {
+        dependent: NonEmpty<I>,
+    },
+}
+
 fn main() {
     // let guard = pprof::ProfilerGuardBuilder::default().frequency(1000).blocklist(&["libc", "libgcc", "pthread", "vdso"]).build().unwrap();
     let f = |k: i32| {
@@ -53,6 +120,29 @@ fn main() {
             CacheAll::new(),
         )
     );
+
+    {
+        let dp = simple_dp(
+            |k: i32| {
+                if k == 0 {
+                    PartialProblemState::Base {
+                        base_result: 0
+                    }
+                } else if k == 1 {
+                    PartialProblemState::Base {
+                        base_result: 1
+                    }
+                } else {
+                    PartialProblemState::Intermediate {
+                        dependent: ne_vec![k - 1, k - 2]
+                    }
+                }
+            },
+            Sum::new(),
+        );
+        // FIXME: off-by-one error?
+        println!("{}", run_print_time("the dp function", || dp.dp(31)));
+    }
     /*
     match guard.report().build() {
         Ok(report) => {
