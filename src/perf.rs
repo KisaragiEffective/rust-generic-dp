5ffeb9ec8effd9caa660c93c290bf5f897624370
KisaragiEffective <kisaragi.effective@gmail.com>
2022-06-17 02:02:57 +0900

    feat: 制約を緩める

--- src/perf.rs
+++ src/perf.rs
@@ -1,11 +1,11 @@
 use std::fmt::Display;
 use std::time::Instant;
 
-pub fn run_print_time<'a, F: 'a + FnOnce() -> T, T>(message: &'a impl Display, f: F) -> T {
+pub fn run_print_time<'a, F: 'a + FnOnce() -> T, T>(message: &'a (impl Display + ?Sized), f: F) -> T {
     let start = Instant::now();
     let t = f();
     let duration = start.elapsed();
 
-    println!("Time elapsed in {str} is: {:?}", duration);
+    println!("Time elapsed in {message} is: {:?}", duration);
     t
 }
