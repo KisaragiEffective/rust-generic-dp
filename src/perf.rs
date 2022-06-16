1dd6a3b755a34a9dd5b7b4e0af1dc6a4a19523c4
KisaragiEffective <kisaragi.effective@gmail.com>
2022-06-17 01:27:52 +0900

    refactor: generify

--- src/perf.rs
+++ src/perf.rs
@@ -1,6 +1,7 @@
+use std::fmt::Display;
 use std::time::Instant;
 
-pub fn run_print_time<'a, F: 'a + FnOnce() -> T, T>(str: &'a str, f: F) -> T {
+pub fn run_print_time<'a, F: 'a + FnOnce() -> T, T>(message: &'a impl Display, f: F) -> T {
     let start = Instant::now();
     let t = f();
     let duration = start.elapsed();
