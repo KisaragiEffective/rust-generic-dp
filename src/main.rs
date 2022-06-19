0f14ec92cb1c1681bd9c465b11b43784d8702233
KisaragiEffective <kisaragi.effective@gmail.com>
2022-06-19 19:56:05 +0900

    test: add honest dp, with memoize

--- src/main.rs
+++ src/main.rs
@@ -69,7 +69,27 @@ fn main() {
             ),
             Sum::new(),
         );
-        println!("{}", run_print_time("simple dp w/o memoize", || dp.dp(30)));
+        println!("{}", run_print_time("simple dp w/o memoize, Sum struct", || dp.dp(30)));
+    }
+
+    {
+        let dp = dp::simple_dp(
+            SolverFactory::function(
+                |k: i32| {
+                    if k == 0 || k == 1 {
+                        State::Base {
+                            base_result: 1
+                        }
+                    } else {
+                        State::Intermediate {
+                            dependent: ne_vec![k - 1, k - 2]
+                        }
+                    }
+                }
+            ),
+            |a, b| a + b,
+        );
+        println!("{}", run_print_time("simple dp w/o memoize, function reducer", || dp.dp(30)));
     }
 
     {
@@ -135,6 +155,21 @@ fn main() {
             Sum::new(),
         );
         println!("{}", run_print_time("simple dp w/ cache by array", || dp.dp(30)));
+    }
+
+    {
+        let res = run_print_time("honest dp w/ cache by array", || {
+            let mut dp_cache = [0u64; 31];
+            dp_cache[0] = 1;
+            dp_cache[1] = 1;
+            for i in 2..31 {
+                dp_cache[i] = dp_cache[i - 1] + dp_cache[i - 2];
+            }
+
+            dp_cache[30]
+        });
+
+        println!("{res}");
     }
 }
 
