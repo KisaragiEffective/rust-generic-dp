38ba487d093b5c337343dd08be841b290fd37034
KisaragiEffective <kisaragi.effective@gmail.com>
2022-06-23 15:03:52 +0900

    docs: add documentation.

--- src/dp/state.rs
+++ src/dp/state.rs
@@ -1,6 +1,9 @@
 use std::ops::Deref;
-
+#[allow(clippy::module_name_repetitions)]
 pub trait StateExtractor<T> {
+    /// returns computed result.
+    /// if computed, the this method returns Some(result).
+    /// If not, returns None.
     fn get_value(&self) -> Option<T>;
 }
 
